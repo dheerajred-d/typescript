6628b2badf18a849204cfd92c3364eba08e20086
dheerajred-d <dheerajreddy@amc-studio.com>
2018-03-29 16:42:23 +0530

    module create

---
+++ moduleCreate/newone.js
@@ -0,0 +1,7 @@
+var A6 = /** @class */ (function () {
+    function A6(audi) {
+    }
+    return A6;
+}());
+var audi = new Audi();
+var a6 = new A6();
---
+++ moduleCreate/newone.ts
@@ -0,0 +1,12 @@
+
+export class Audi {
+
+}
+class A6 {
+    constructor(audi? : Audi){
+
+    }
+}
+
+let audi : Audi = new Audi();
+let a6 : A6 = new A6();
